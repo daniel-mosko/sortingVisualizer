9bb40c003af36c785a757309eeb06f29406f552d
daniel-mosko <59666997+daniel-mosko@users.noreply.github.com>
2021-07-04 14:04:21 +0200

    up

--- js/sorting.js
+++ js/sorting.js
@@ -2,6 +2,8 @@ const array = [];
 
 document.getElementById("new-array").addEventListener("click", setSize);
 document.getElementById("bubble-sort").addEventListener("click", bubbleSort);
+document.getElementById("shaker-sort").addEventListener("click", shakerSort);
+
 
 
 function setSize() {
@@ -14,21 +16,27 @@ function generateArray(size) {
     for (let i = 0; i < size; i++) {
         var num = Math.floor(Math.random() * 500) + 1;
         // Nemôžu byť duplikáty
-        if(!array.includes(num)){
+        if (!array.includes(num)) {
+            array.push(num);
+        }
+        else{
+            while(array.includes(num)){
+                var num = Math.floor(Math.random() * 500) + 1;
+            }
             array.push(num);
         }
     }
     for (let i = 0; i < array.length; i++) {
         const arrayElement = document.createElement('div');
         var height = array[i].toString();
-        console.log(array[i]);
         arrayElement.style.height = height + "px";
         arrayElement.innerHTML = "";
         arrayElement.classList.add("array-bar");
         arrayElement.setAttribute("id", array[i].toString());
         document.getElementById("array-container").appendChild(arrayElement);
     }
-    return array;   
+    console.log(array.length);
+    return array;
 }
 
 function deleteArray() {
@@ -42,19 +50,18 @@ function deleteArray() {
 async function bubbleSort() {
     disableButtons(true);
     const timer = ms => new Promise(res => setTimeout(res, ms))
-    console.log(array);
     var i, j;
     for (i = 0; i < array.length; i++) {
         for (j = 0; j < array.length - 1; j++) {
             if (array[i] < array[j]) {
-                
+
                 const b1 = document.getElementById(array[i].toString()); // i
                 const b2 = document.getElementById(array[j].toString()); // j
 
                 b1.style.backgroundColor = "red";
                 b2.style.backgroundColor = "red";
                 await timer(1);
-                
+
 
                 var ts = b1.style.height;
                 var ti = b1.id;
@@ -72,15 +79,47 @@ async function bubbleSort() {
             }
         }
     }
+
     disableButtons(false);
 }
 
-async function mergeSort(){
-    
+async function shakerSort() {
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+    var i, j;
+    for (i = array.length; i >= 0; i--) {
+        for (j = array.length; j > 0; j--) {
+            if (array[i] > array[j]) {
+
+                const b1 = document.getElementById(array[i].toString());
+                const b2 = document.getElementById(array[j].toString());
+
+                b1.style.backgroundColor = "red";
+                b2.style.backgroundColor = "red";
+                await timer(1);
+
+                var ts = b1.style.height;
+                var ti = b1.id;
+                b1.style.height = b2.style.height;
+                b1.id = b2.id;
+                b2.style.height = ts;
+                b2.id = ti;
+
+                b1.style.backgroundColor = "rgb(180, 194, 255)";
+                b2.style.backgroundColor = "rgb(180, 194, 255)";
+
+                var temp = array[i]
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+
+    disableButtons(false);
 }
 
-function disableButtons(bool){
+function disableButtons(bool) {
     document.getElementById("new-array").disabled = bool;
     document.getElementById("bubble-sort").disabled = bool;
     document.getElementById("array-slider").disabled = bool;
+    document.getElementById("shaker-sort").disabled = bool;
 }
